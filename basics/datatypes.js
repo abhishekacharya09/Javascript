fb6b85da99e197b2115a54548439e05e647e50ca
Abhishek Acharya <108420417+abhishekacharya09@users.noreply.github.com>
2023-12-20 11:46:46 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,20 @@
+"use strict" // Treat all js code as new version
+// alert('hi'); // we are using nodejs not browser
+
+let name = "Abhishek";
+let age = 22;
+let working = true;
+
+// number 2 to 2 power 53 is the range;
+// string = ""
+// boolean = false,true
+// null = standalone value
+// undefined = value is not assigned
+// symbol = unique
+
+
+// Objects
+
+console.log(typeof(undefined)) //undefined
+console.log(typeof(null)) //object
+console.log(typeof(12)) //number
